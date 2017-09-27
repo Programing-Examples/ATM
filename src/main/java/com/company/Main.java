6d96f38e2c0794a5005e4e90090800d21108d941
01omar10 <omardiaz88@gmail.com>
2017-09-28 01:18:48 +0200

    [ATM-1] ATM using simple if's

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,41 @@
+package main.java.com.company;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        short option;
+        double currentBalance = 1000;
+
+        System.out.println("\t\t\t\t\t Credit Banco");
+        System.out.println("\t************************************************");
+        System.out.println("\t* Elija una opcion y presione Enter:           *");
+        System.out.println("\t* 1. Consultar saldo                           *");
+        System.out.println("\t* 2. Retirar dinero                            *");
+        System.out.println("\t* 3. Ingresar dinero                           *");
+        System.out.println("\t************************************************");
+
+        Scanner scanner = new Scanner(System.in);
+        option = scanner.nextShort();
+
+        if(option > 3 || option < 1) {
+            System.out.println("Invalid Option ");
+        }
+        if(option == 1) {
+            System.out.println("Saldo: " + currentBalance);
+        }
+        if(option == 2) {
+            System.out.println("Cuanto desea retirar: ");
+            currentBalance -= scanner.nextDouble();
+            System.out.println("Saldo: " + currentBalance);
+        }
+        if(option == 3) {
+            System.out.println("Cuanto desea ingresar: ");
+            currentBalance += scanner.nextDouble();
+            System.out.println("Saldo: " + currentBalance);
+        }
+
+    }
+}
